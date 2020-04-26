e4e7cbda20b9768e6bd47d65c1460b9732ca4a7e
Nhieu Nguyen <nhieunguyen@kms-technology.com>
2020-04-26 15:47:49 +0700

    Bai tap tuan 10

--- BaiTapTuan08/Program.cs
+++ BaiTapTuan08/Program.cs
@@ -5,206 +5,122 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BaiTapTuan08
+namespace BaiTapTuan10
 {
     class Program
     {
         static void Main(string[] args)
         {
             List<List<int>> graph = new List<List<int>>();
+            int size;
+            int start;
             // Open the text file using a stream reader.
             using (StreamReader sr = new StreamReader("input.txt"))
             {
                 // Read the stream to a string, and write the string to the console.
-                int size = Int32.Parse(sr.ReadLine());
+                string[] firstLine = sr.ReadLine().Split();
+                size = Int32.Parse(firstLine[0]);
+                start = Int32.Parse(firstLine[1]);
+                Int32.Parse("-4");
                 for (int i = 0; i < size; i++)
                 {
                     List<int> row = new List<int>();
                     string line = sr.ReadLine();
                     string[] values = line.Split(' ');
-                    Array.ForEach(values, v => row.Add(Int32.Parse(v)));
+                    Array.ForEach(values, v =>
+                    {
+                        row.Add(Int32.Parse(v));
+                    });
                     graph.Add(row);
                 }
             }
-            int eulerType = Program.checkEulerGraph(graph);
-            string statement = "";
-            string path = "";
-            if (eulerType == Program.EULER_GRAPH_TYPE.NOT_EULER)
+
+            int[,] costs = new int[size + 1, size];
+            int[,] from = new int[size, size];
+            for (int i = 0; i < size + 1; i++)
             {
-                statement = "Do thi khong co tinh chat Euler";
-            }
-            else if (eulerType == Program.EULER_GRAPH_TYPE.HALF_EULER)
-            {
-                statement = "Day la do thi nua Euler";
-                path = "Duong di Euler: ";
-                List<int> oddNodes = new List<int>();
-                int size = graph.Count;
-                for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
                 {
-                    int deg = graph[i].Count(isEdge => isEdge == 1);
-                    if (deg % 2 != 0)
+                    costs[i, j] = Int32.MaxValue;
+                }
+            }
+            costs[0, start] = 0;
+            Queue<List<int>> queue = new Queue<List<int>>();
+            queue.Enqueue(new List<int>() { start });
+            for (int i = 0; i < size; i++)
+            {
+                List<int> nodes = queue.Dequeue();
+                for (int j = 0; j < size; j++)
+                {
+                    costs[i + 1, j] = costs[i, j];
+                }
+                nodes.ForEach(node =>
+                {
+                    var paths = graph[node].Select((value, index) => new { value, index })
+                             .Where(z => z.value != 0);
+                    bool[] changed = new bool[size];
+                    //List<int> changed = new List<int>();
+                    foreach(var path in paths)
                     {
-                        oddNodes.Add(i);
+                        int value = path.value;
+                        int index = path.index;
+                        if (costs[i, index] == Int32.MaxValue || value + costs[i, node] < costs[i, index])
+                        {
+                            costs[i + 1, index] = value + costs[i, node];
+                            from[i, index] = node;
+                            changed[index] = true;
+                        }
+                        //else if ()
+                        //{
+                        //    costs[i + 1, index] = value + costs[i, node];
+                        //    from[i, index] = node;
+                        //    changed[index] = true;
+                        //}
                     }
-                    if (oddNodes.Count == 2)
+                    List<int> changedList = changed.Select((value, index) => new { value, index })
+                             .Where(z => z.value == true)
+                             .Select(z => z.index).ToList<int>();
+                    queue.Enqueue(changedList);
+                });
+                for (int j = 0; j < size; j++)
+                {
+                    if (i < size - 1 && i > 0)
                     {
-                        break;
+                        from[i + 1, j] = from[i, j];
                     }
                 }
-                List<int> eulerCycle = Program.findEulerCycle(graph, oddNodes[0]);
-                path += String.Join(" ", eulerCycle.ToArray());
+            }
+            bool hasNegativeCycle = true;
+            string output = "";
+            for (int i = 0; i < size; i++)
+            {
+                if (costs[size, i] == Int32.MaxValue)
+                {
+                    output += $"Duong di ngan nhat tu {start} den {i}: Khong co duong di\n";
+                }
+                else if (i != start)
+                {
+                    hasNegativeCycle = false;
+                    List<int> shortestPath = new List<int>() { i };
+                    int prevNode = from[size - 1, i];
+                    shortestPath.Add(prevNode);
+                    while(prevNode != start)
+                    {
+                        prevNode = from[size - 1, prevNode];
+                        shortestPath.Add(prevNode);
+                    }
+                    output += $"Duong di ngan nhat tu {start} den {i}: {String.Join(" <- ", shortestPath.ToArray())}\n";
+                    output += $"Chi phi duong di ngan nhat: {costs[size, i]}\n";
+                }
+            }
+            if (hasNegativeCycle)
+            {
+                Console.WriteLine("Do thi co mach am");
             }
             else
             {
-                statement = "Day la do thi Euler.";
-                path = "Chu trinh Euler: ";
-                List<int> eulerCycle = Program.findEulerCycle(graph);
-                path += String.Join(" ", eulerCycle.ToArray());
+                Console.WriteLine(output);
             }
-            Console.WriteLine(statement);
-            Console.WriteLine("{0}", path);
-        }
-
-        static List<int> findEulerCycle(List<List<int>> graph, int from = -1)
-        {
-            int size = graph.Count;
-            List<List<int>> graphAfterVisit = Program.cloneGraph(graph);
-            int startNode = from;
-            for (int i = 0; i < size; i++)
-            {
-                if (startNode != -1)
-                {
-                    break;
-                }
-                for (int j = 0; j < size; j++)
-                {
-                    if (graph[i][j] == 1)
-                    {
-                        startNode = i;
-                        break;
-                    }
-                }
-            }
-            List<int> eulerCycle = new List<int>();
-            int currentNode = startNode;
-            while (currentNode != -1)
-            {
-                eulerCycle.Add(currentNode);
-                int nextNode = -1;
-                for (int i = 0; i < size; i++)
-                {
-                    if (graphAfterVisit[currentNode][i] == 1)
-                    {
-                        nextNode = i;
-                        List<List<int>> cloned = Program.cloneGraph(graphAfterVisit);
-                        cloned[currentNode][i] = 0;
-                        cloned[i][currentNode] = 0;
-                        BFS bfsIns = new BFS(cloned);
-                        if (bfsIns.checkNodeConnection(currentNode, i))
-                        {
-                            break;
-                        }
-                    }
-                }
-                if (nextNode != -1)
-                {
-                    graphAfterVisit[currentNode][nextNode] = 0;
-                    graphAfterVisit[nextNode][currentNode] = 0;
-                }
-                currentNode = nextNode;
-            }
-            return eulerCycle;
-        }
-
-
-        static int checkEulerGraph(List<List<int>> graph)
-        {
-            int size = graph.Count;
-            int oddVertexCount = 0;
-            for (int i = 0; i < size; i++)
-            {
-                int deg = graph[i].Count(isEdge => isEdge == 1);
-                if (deg % 2 != 0)
-                {
-                    oddVertexCount++;
-                }
-                if (oddVertexCount > 2)
-                {
-                    break;
-                }
-            }
-            if (oddVertexCount == 0)
-            {
-                return Program.EULER_GRAPH_TYPE.COMPLETE_EULER;
-            }
-
-            if (oddVertexCount <= 2)
-            {
-                return Program.EULER_GRAPH_TYPE.HALF_EULER;
-            }
-            return Program.EULER_GRAPH_TYPE.NOT_EULER;
-        }
-
-        static public List<List<int>> cloneGraph(List<List<int>> graph)
-        {
-            int size = graph.Count;
-            List<List<int>> cloned = new List<List<int>>();
-            for (int i = 0; i < size; i++)
-            {
-                cloned.Add(new List<int>());
-                for (int j = 0; j < size; j++)
-                {
-                    cloned[i].Add(graph[i][j]);
-                }
-            }
-            return cloned;
-        }
-        public class BFS
-        {
-            public List<List<int>> graph;
-            public int size;
-            public BFS(List<List<int>> graph)
-            {
-                size = graph.Count;
-                this.graph = graph;
-            }
-            public bool checkNodeConnection(int startNode, int endNode)
-            {
-                List<int> visitedNodes = new List<int>();
-                Queue<int> bfsQueue = new Queue<int>();
-                bfsQueue.Enqueue(startNode);
-                visitedNodes.Add(startNode);
-                bool isConnected = false;
-                while(bfsQueue.Count != 0)
-                {
-                    int currentNode = bfsQueue.Dequeue();
-                    for (int i = 0; i < size; i++)
-                    {
-                        if (i == endNode && graph[currentNode][i] == 1)
-                        {
-                            isConnected = true;
-                            break;
-                        }
-                        if (graph[currentNode][i] == 1 && !visitedNodes.Any(node => node == i))
-                        {
-                            visitedNodes.Add(i);
-                            bfsQueue.Enqueue(i);
-                        }
-                    }
-                    if (isConnected)
-                    {
-                        break;
-                    }
-                }
-                return isConnected;
-            }
-        }
-
-        static public class EULER_GRAPH_TYPE
-        {
-            static public int COMPLETE_EULER = 1;
-            static public int HALF_EULER = 0;
-            static public int NOT_EULER = -1;
         }
     }
 }
